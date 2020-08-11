9bf9e6445bb67692d09ec39c139e9e3c92de8173
Josh Wong <jbwong05@gmail.com>
2020-08-10 19:05:54 -0700

    Added watch

---
+++ src/watch/fileutils/fileutils.c
@@ -0,0 +1,44 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "nls.h"
+#include "fileutils.h"
+#ifdef HAVE_ERROR_H
+# include <error.h>
+#else
+# include "c.h" /* for error() emulation */
+#endif
+#ifdef HAVE_STDIO_EXT_H
+# include <stdio_ext.h>
+#else
+/* FIXME: use a more portable definition of __fpending() (from gnulib?) */
+# define __fpending(fp) ((fp)->_p - (fp)->_bf._base)
+#endif
+
+int close_stream(FILE * stream)
+{
+	const int some_pending = (__fpending(stream) != 0);
+	const int prev_fail = (ferror(stream) != 0);
+	const int fclose_fail = (fclose(stream) != 0);
+	if (prev_fail || (fclose_fail && (some_pending || errno != EBADF))) {
+		if (!fclose_fail && errno != EPIPE)
+			errno = 0;
+		return EOF;
+	}
+	return 0;
+}
+
+/* Use atexit(); */
+void close_stdout(void)
+{
+	if (close_stream(stdout) != 0 && !(errno == EPIPE)) {
+		char const *write_error = _("write error");
+		error(0, errno, "%s", write_error);
+		_exit(EXIT_FAILURE);
+	}
+
+	if (close_stream(stderr) != 0)
+		_exit(EXIT_FAILURE);
+}
